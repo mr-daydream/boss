9e7c909fb5ed7596ed62f195b9773fbf63c56369
mr-daydream <luke.samuels@gmail.com>
2017-04-29 16:41:20 -0400

    Added bend as a metric type to represent a real range

--- scripts/dummydata.sh
+++ scripts/dummydata.sh
@@ -3,9 +3,12 @@
 GRAPH_SERVER='104.131.45.105'
 GRAPH_PORT='2003'
 METRIC_NAME=$1
+DATA_TYPE=$2
 
-while [ 1 -eq 1 ]; do
-        echo "$METRIC_NAME $(( RANDOM % (10 - 5 + 1 ) + 5 )) `date +%s`" | nc -c $GRAPH_SERVER $GRAPH_PORT;
-    sleep 1;
-done;
+if [ $DATA_TYPE == "bend" ]; then
+    while [ 1 -eq 1 ]; do
+        echo "$METRIC_NAME $(( RANDOM % (947 - 730 + 1 ) + 730 )) `date +%s`" | nc $GRAPH_SERVER $GRAPH_PORT; #If running on a mac will need -c in the nc command
+        sleep 1;
+    done;
+fi
 
