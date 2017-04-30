e6c38efdfc33cbc83c0c7eb856dc2d39fcdf24d2
mr-daydream <luke.samuels@gmail.com>
2017-04-30 11:56:58 -0400

    Final commit before judging

--- collector/parinput.py
+++ collector/parinput.py
@@ -4,30 +4,25 @@ import time
 import serial
 
 ser = serial.Serial('/dev/ttyS0')
-#raw_data = ser.readline()
-#print raw_data
 
 CARBON_SERVER = '104.131.45.105'
 CARBON_PORT = 2003
 array = []
-#with open('sampleoutput.txt') as f:
-#        content = f.read().splitlines()
-#        for line in content:
 while True:
         line = ser.readline()
         if len(line) == 51:
             mydict = dict(item.split(':') for item in line.split())
             tempmsg = "center1.bed%s.temp %s %i\n" % (mydict['N'][-1:], mydict['T'], int(time.time())) 
             print 'sending message:\n%s' % tempmsg
-       #sock = socket.socket()
-       #sock.connect((CARBON_SERVER, CARBON_PORT))
-       #sock.sendall(tempmsg)
-       #sock.close()
+            sock = socket.socket()
+            sock.connect((CARBON_SERVER, CARBON_PORT))
+            sock.sendall(tempmsg)
+            sock.close()
             bendmsg = "center1.bed%s.bend %s %i\n" % (mydict['N'][-1:], mydict['B'], int(time.time())) 
             print 'sending message:\n%s' % bendmsg
-       #sock = socket.socket()
-       #sock.connect((CARBON_SERVER, CARBON_PORT))
-       #sock.sendall(bendmsg)
-       #sock.close()
+            sock = socket.socket()
+            sock.connect((CARBON_SERVER, CARBON_PORT))
+            sock.sendall(bendmsg)
+            sock.close()
             time.sleep(1)
 ser.close()
